6dd57e12581b9cfdc00e92bd9b834f2b30ef3694
sy-records <52o@qq52o.cn>
2023-07-28 20:53:54 +0800

    Add typecho ci

--- Plugin.php
+++ Plugin.php
@@ -1,18 +1,30 @@
 <?php
 /**
- * 「特殊节日使用」在国家公祭日、全国哀悼日时网站增加灰色滤镜。<a href="https://github.com/sy-records/MemorialDay" target="_blank">Github</a>
+ * 「特殊节日使用」在国家公祭日、全国哀悼日时网站增加灰色滤镜。<a href="https://github.com/sy-records/MemorialDay" target="_blank">GitHub</a>
  *
  * @package MemorialDay
  * @author 沈唁
- * @version 1.0.0
+ * @version 1.0.3
  * @link https://qq52o.me
  */
 
-class MemorialDay_Plugin implements Typecho_Plugin_Interface
+namespace TypechoPlugin\MemorialDay;
+
+use Typecho\Widget;
+use Typecho\Plugin as TypechoPlugin;
+use Typecho\Plugin\PluginInterface;
+use Typecho\Widget\Helper\Form;
+use Typecho\Widget\Helper\Form\Element\Text;
+
+if (!defined('__TYPECHO_ROOT_DIR__')) {
+    exit;
+}
+
+class Plugin implements PluginInterface
 {
     public static function activate()
     {
-        Typecho_Plugin::factory('Widget_Archive')->header = array(__CLASS__, 'website_set');
+        TypechoPlugin::factory('Widget_Archive')->header = array(__CLASS__, 'websiteSet');
         return _t('MemorialDay 插件已启用');
     }
 
@@ -21,9 +33,9 @@ class MemorialDay_Plugin implements Typecho_Plugin_Interface
         return _t('MemorialDay 插件已禁用');
     }
 
-    public static function config(Typecho_Widget_Helper_Form $form)
+    public static function config(Form $form)
     {
-        $days = new Typecho_Widget_Helper_Form_Element_Text(
+        $days = new Text(
             'days',
             null,
             "0404,0512,0918,1213",
@@ -33,15 +45,15 @@ class MemorialDay_Plugin implements Typecho_Plugin_Interface
         $form->addInput($days->addRule('required', _t('日期为必填项')));
     }
 
-    public static function personalConfig(Typecho_Widget_Helper_Form $form)
+    public static function personalConfig(Form $form)
     {
     }
 
-    public static function website_set()
+    public static function websiteSet()
     {
-        $days = Typecho_Widget::widget('Widget_Options')->plugin('MemorialDay')->days;
-        $day_arr = explode(",", $days);
-        if (in_array(date('md'), $day_arr)) {
+        $days = Widget::widget('Widget_Options')->plugin('MemorialDay')->days;
+        $dayArr = explode(',', $days);
+        if (in_array(date('md'), $dayArr)) {
             echo "<style type='text/css'>html{ filter: grayscale(100%); -webkit-filter: grayscale(100%); -moz-filter: grayscale(100%); -ms-filter: grayscale(100%); -o-filter: grayscale(100%); filter: url('data:image/svg+xml;utf8,#grayscale'); filter:progid:DXImageTransform.Microsoft.BasicImage(grayscale=1); -webkit-filter: grayscale(1);}</style>
 ";
         }
